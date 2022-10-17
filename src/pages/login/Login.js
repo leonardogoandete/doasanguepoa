e5d6c1f536dbe5345764b7ddd2dd996329270b52
leonardo_goandete <leonardo_goandete@sicredi.com.br>
2022-10-17 20:12:32 -0300

    Ajuste de endpoint

--- src/pages/login/Login.js
+++ src/pages/login/Login.js
@@ -9,7 +9,7 @@ import './Login.css'
 
 const Login = () => {
     const handleSubmit = values => {
-        axios.post('http://doasanguepoa-bff.herokuapp.com/v1/api/usuario/login', values)
+        axios.post('http://doasanguepoa-bff.herokuapp.com/v1/api/usuarios/login', values)
         //axios.post('http://localhost:3333/usuario/login', values)
             .then(resp => {
                 const { data } = resp
