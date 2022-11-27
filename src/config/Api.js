e295bca6c153e7ffab6995934a22e16ae81b5289
leonardogoandete <leonardogoandete>
2022-11-27 18:25:39 -0300

    testando nova baseURL

--- src/config/Api.js
+++ src/config/Api.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 const token = localStorage.getItem('u')
 export const Api = axios.create({
-    baseURL: "https://doasanguepoa-bff.herokuapp.com/v1/api/",
+    //baseURL: "https://doasanguepoa-bff.herokuapp.com/v1/api/",
+    baseURL: "https://doasanguepoa-bff-aharx.ondigitalocean.app/v1/api/",
     headers: {'x-access-token': token}
 });
