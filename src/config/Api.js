a7fc339816946204ebd0b9a74d8978fe2bd3ca41
leonardo_goandete <leonardo_goandete>
2023-11-05 14:27:31 -0300

    Ajustado configuração para gerar imagem e para build

--- src/config/Api.js
+++ src/config/Api.js
@@ -2,8 +2,7 @@ import axios from "axios";
 require('dotenv').config();
 
 export const Api = axios.create(
-    console.log(process.env.TESTE),
     {
-        baseURL: "http://localhost:8080/"
+        baseURL: process.env.REACT_APP_URL_API
     }
 );
