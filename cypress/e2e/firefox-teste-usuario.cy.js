ee8a521d531455c13de9573131cca59a9106a164
leonardo_goandete <leonardo_goandete>
2023-11-10 13:19:31 -0300

    Teste

--- cypress/e2e/firefox-teste-usuario.cy.js
+++ cypress/e2e/firefox-teste-usuario.cy.js
@@ -7,7 +7,7 @@ describe('template spec', () => {
       // Interceptação para a chamada POST /auth/login
       cy.intercept({
         method: 'POST',
-        url: 'http://localhost:8082/auth/login',
+        url: '**/auth/login',
       }, {
         statusCode: 200,
         body: loginResponse,
@@ -29,17 +29,12 @@ describe('template spec', () => {
     cy.get('[href="/home"]').click();
     cy.wait('@getPostagens');
 
-    cy.get(':nth-child(1) > .post-body').click();
-    cy.get('[href="/agendamento"]').click();
-
     cy.intercept('GET', '**/postagens', {
       fixture: 'postagens.json', // Crie um arquivo postagens.json na pasta fixtures com os dados que você deseja
     }).as('getPostagens');
     cy.get('[href="/home"]').click();
     cy.wait('@getPostagens');
     
-    cy.get('.rs-navbar-right > .rs-navbar-item > .rs-navbar-item-icon').click();
-    cy.contains('Login').should('be.visible');
     /* ==== End Cypress Studio ==== */
   });
 });
