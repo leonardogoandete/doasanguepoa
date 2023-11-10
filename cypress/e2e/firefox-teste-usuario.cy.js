70b141cc021f31920b451b10c295f10aa58ed668
leonardo_goandete <leonardo_goandete>
2023-11-10 12:39:12 -0300

    Original cypress

--- cypress/e2e/firefox-teste-usuario.cy.js
+++ cypress/e2e/firefox-teste-usuario.cy.js
@@ -1,50 +1,6 @@
 describe('template spec', () => {
   /* ==== Test Created with Cypress Studio ==== */
-  it('teste-firefox-instituicao', function () {
-    /* ==== Generated with Cypress Studio ==== */
-
-    cy.fixture('loginResponseInstituicao.json').then((loginResponse) => {
-      // Interceptação para a chamada POST /auth/login
-      cy.intercept({
-        method: 'POST',
-        url: '**/auth/login',
-      }, {
-        statusCode: 200,
-        body: loginResponse,
-      }).as('postLogin');
-
-      cy.visit('http://localhost:3000');
-      cy.get(':nth-child(1) > .Login-Field').type('87020517000120');
-      cy.get(':nth-child(2) > .Login-Field').type('clinicas');
-      cy.get('.Login-Button').click();
-
-      // Aguarda a interceptação da chamada POST /auth/login
-      cy.wait('@postLogin');
-    });
-
-    // Mockando a resposta da API para a rota /postagens/instituicao usando cy.fixture
-    cy.fixture('minhas-postagens.json').then((postagens) => {
-      cy.intercept('GET', '**/postagens/instituicao', { body: postagens }).as('getMinhasPostagens');
-    });
-
-    cy.get('[href="/minhas-postagens"]').click();
-
-    // Aguarda a interceptação da chamada da API
-    cy.wait('@getMinhasPostagens');
-
-    cy.intercept('GET', '**/postagens', {
-      fixture: 'postagens.json', // Crie um arquivo postagens.json na pasta fixtures com os dados que você deseja
-    }).as('getPostagens');
-    cy.get('[href="/home"]').click();
-    cy.wait('@getPostagens');
-
-    cy.get('.rs-navbar-right > .rs-navbar-item > .rs-navbar-item-icon').click();
-    cy.contains('Login').should('be.visible');
-    /* ==== End Cypress Studio ==== */
-  });
-
-
-  it('teste-firefox-usuario', function () {
+  it('teste-firefox', function () {
     /* ==== Generated with Cypress Studio ==== */
     // Mockando a resposta da API de login usando cy.fixture
     cy.fixture('loginResponseUsuario.json').then((loginResponse) => {
@@ -86,5 +42,4 @@ describe('template spec', () => {
     cy.contains('Login').should('be.visible');
     /* ==== End Cypress Studio ==== */
   });
-
 });
