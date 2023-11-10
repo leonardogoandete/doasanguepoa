57d37f9478c130d9e829ee8e791091f8b87022fb
leonardo_goandete <leonardo_goandete>
2023-11-10 12:28:18 -0300

    Teste

--- cypress/e2e/firefox-teste-usuario.cy.js
+++ cypress/e2e/firefox-teste-usuario.cy.js
@@ -1,45 +1,45 @@
-describe('template spec', () => {
-  /* ==== Test Created with Cypress Studio ==== */
-  it('teste-firefox', function () {
-    /* ==== Generated with Cypress Studio ==== */
-    // Mockando a resposta da API de login usando cy.fixture
-    cy.fixture('loginResponseUsuario.json').then((loginResponse) => {
-      // Interceptação para a chamada POST /auth/login
-      cy.intercept({
-        method: 'POST',
-        url: 'http://localhost:8082/auth/login',
-      }, {
-        statusCode: 200,
-        body: loginResponse,
-      }).as('postLogin');
+// describe('template spec', () => {
+//   /* ==== Test Created with Cypress Studio ==== */
+//   it('teste-firefox', function () {
+//     /* ==== Generated with Cypress Studio ==== */
+//     // Mockando a resposta da API de login usando cy.fixture
+//     cy.fixture('loginResponseUsuario.json').then((loginResponse) => {
+//       // Interceptação para a chamada POST /auth/login
+//       cy.intercept({
+//         method: 'POST',
+//         url: 'http://localhost:8082/auth/login',
+//       }, {
+//         statusCode: 200,
+//         body: loginResponse,
+//       }).as('postLogin');
 
-      cy.visit('http://localhost:3000');
-      cy.get(':nth-child(1) > .Login-Field').type('12345678912');
-      cy.get(':nth-child(2) > .Login-Field').type('leonardo');
-      cy.get('.Login-Button').click();
+//       cy.visit('http://localhost:3000');
+//       cy.get(':nth-child(1) > .Login-Field').type('12345678912');
+//       cy.get(':nth-child(2) > .Login-Field').type('leonardo');
+//       cy.get('.Login-Button').click();
 
-      // Aguarda a interceptação da chamada POST /auth/login
-      cy.wait('@postLogin');
-    });
+//       // Aguarda a interceptação da chamada POST /auth/login
+//       cy.wait('@postLogin');
+//     });
 
-    cy.intercept('GET', '**/postagens', {
-      fixture: 'postagens.json', // Crie um arquivo postagens.json na pasta fixtures com os dados que você deseja
-    }).as('getPostagens');
+//     cy.intercept('GET', '**/postagens', {
+//       fixture: 'postagens.json', // Crie um arquivo postagens.json na pasta fixtures com os dados que você deseja
+//     }).as('getPostagens');
 
-    cy.get('[href="/home"]').click();
-    cy.wait('@getPostagens');
+//     cy.get('[href="/home"]').click();
+//     cy.wait('@getPostagens');
 
-    cy.get(':nth-child(1) > .post-body').click();
-    cy.get('[href="/agendamento"]').click();
+//     cy.get(':nth-child(1) > .post-body').click();
+//     cy.get('[href="/agendamento"]').click();
 
-    cy.intercept('GET', '**/postagens', {
-      fixture: 'postagens.json', // Crie um arquivo postagens.json na pasta fixtures com os dados que você deseja
-    }).as('getPostagens');
-    cy.get('[href="/home"]').click();
-    cy.wait('@getPostagens');
+//     cy.intercept('GET', '**/postagens', {
+//       fixture: 'postagens.json', // Crie um arquivo postagens.json na pasta fixtures com os dados que você deseja
+//     }).as('getPostagens');
+//     cy.get('[href="/home"]').click();
+//     cy.wait('@getPostagens');
     
-    cy.get('.rs-navbar-right > .rs-navbar-item > .rs-navbar-item-icon').click();
-    cy.contains('Login').should('be.visible');
-    /* ==== End Cypress Studio ==== */
-  });
-});
+//     cy.get('.rs-navbar-right > .rs-navbar-item > .rs-navbar-item-icon').click();
+//     cy.contains('Login').should('be.visible');
+//     /* ==== End Cypress Studio ==== */
+//   });
+// });
---
+++ cypress/e2e/firefox-teste.cy.js
@@ -0,0 +1,90 @@
+describe('template spec', () => {
+  /* ==== Test Created with Cypress Studio ==== */
+  it('teste-firefox-instituicao', function () {
+    /* ==== Generated with Cypress Studio ==== */
+
+    cy.fixture('loginResponseInstituicao.json').then((loginResponse) => {
+      // Interceptação para a chamada POST /auth/login
+      cy.intercept({
+        method: 'POST',
+        url: '**/auth/login',
+      }, {
+        statusCode: 200,
+        body: loginResponse,
+      }).as('postLogin');
+
+      cy.visit('http://localhost:3000');
+      cy.get(':nth-child(1) > .Login-Field').type('87020517000120');
+      cy.get(':nth-child(2) > .Login-Field').type('clinicas');
+      cy.get('.Login-Button').click();
+
+      // Aguarda a interceptação da chamada POST /auth/login
+      cy.wait('@postLogin');
+    });
+
+    // Mockando a resposta da API para a rota /postagens/instituicao usando cy.fixture
+    cy.fixture('minhas-postagens.json').then((postagens) => {
+      cy.intercept('GET', '**/postagens/instituicao', { body: postagens }).as('getMinhasPostagens');
+    });
+
+    cy.get('[href="/minhas-postagens"]').click();
+
+    // Aguarda a interceptação da chamada da API
+    cy.wait('@getMinhasPostagens');
+
+    cy.intercept('GET', '**/postagens', {
+      fixture: 'postagens.json', // Crie um arquivo postagens.json na pasta fixtures com os dados que você deseja
+    }).as('getPostagens');
+    cy.get('[href="/home"]').click();
+    cy.wait('@getPostagens');
+
+    cy.get('.rs-navbar-right > .rs-navbar-item > .rs-navbar-item-icon').click();
+    cy.contains('Login').should('be.visible');
+    /* ==== End Cypress Studio ==== */
+  });
+
+
+  it('teste-firefox-usuario', function () {
+    /* ==== Generated with Cypress Studio ==== */
+    // Mockando a resposta da API de login usando cy.fixture
+    cy.fixture('loginResponseUsuario.json').then((loginResponse) => {
+      // Interceptação para a chamada POST /auth/login
+      cy.intercept({
+        method: 'POST',
+        url: 'http://localhost:8082/auth/login',
+      }, {
+        statusCode: 200,
+        body: loginResponse,
+      }).as('postLogin');
+
+      cy.visit('http://localhost:3000');
+      cy.get(':nth-child(1) > .Login-Field').type('12345678912');
+      cy.get(':nth-child(2) > .Login-Field').type('leonardo');
+      cy.get('.Login-Button').click();
+
+      // Aguarda a interceptação da chamada POST /auth/login
+      cy.wait('@postLogin');
+    });
+
+    cy.intercept('GET', '**/postagens', {
+      fixture: 'postagens.json', // Crie um arquivo postagens.json na pasta fixtures com os dados que você deseja
+    }).as('getPostagens');
+
+    cy.get('[href="/home"]').click();
+    cy.wait('@getPostagens');
+
+    cy.get(':nth-child(1) > .post-body').click();
+    cy.get('[href="/agendamento"]').click();
+
+    cy.intercept('GET', '**/postagens', {
+      fixture: 'postagens.json', // Crie um arquivo postagens.json na pasta fixtures com os dados que você deseja
+    }).as('getPostagens');
+    cy.get('[href="/home"]').click();
+    cy.wait('@getPostagens');
+    
+    cy.get('.rs-navbar-right > .rs-navbar-item > .rs-navbar-item-icon').click();
+    cy.contains('Login').should('be.visible');
+    /* ==== End Cypress Studio ==== */
+  });
+
+});
