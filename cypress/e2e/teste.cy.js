189b1f0a5c49de6a37a6eb265c47d246f3890603
leonardo_goandete <leonardo_goandete>
2023-11-15 11:52:34 -0300

    Teste pipeline #38

--- cypress/e2e/teste.cy.js
+++ cypress/e2e/teste.cy.js
@@ -1,6 +1,6 @@
 describe('template spec', () => {
   /* ==== Test Created with Cypress Studio ==== */
-  it('teste2', function() {
+  it('listagem-postagem-usuario', function() {
     /* ==== Generated with Cypress Studio ==== */
     cy.visit('http://localhost:3000');
     cy.get(':nth-child(1) > .Login-Field').clear('1');
@@ -9,26 +9,33 @@ describe('template spec', () => {
     cy.get(':nth-child(2) > .Login-Field').type('leonardo');
     cy.get('.Login-Button').click();
     cy.get('.rs-navbar-right > .rs-navbar-item > .rs-navbar-item-icon').click();
-    cy.get(':nth-child(1) > .Login-Field').click();
+    /* ==== End Cypress Studio ==== */
+  });
+
+  /* ==== Test Created with Cypress Studio ==== */
+  it('teste-instituicao', function() {
+    /* ==== Generated with Cypress Studio ==== */
+    cy.visit('http://localhost:3000');
     cy.get(':nth-child(1) > .Login-Field').click();
     cy.get(':nth-child(1) > .Login-Field').clear('8702051700012');
     cy.get(':nth-child(1) > .Login-Field').type('87020517000120');
     cy.get(':nth-child(2) > .Login-Field').clear('c');
     cy.get(':nth-child(2) > .Login-Field').type('clinicas');
     cy.get('.Login-Button').click();
+    cy.get('.format').click();
+    cy.get('.formPostagem').click();
+    cy.get('.format').click();
+    cy.get('.formPostagem').click();
     cy.get('[href="/minhas-postagens"]').click();
     cy.get('[href="/home"]').click();
     cy.get('.formPostagem').click();
     cy.get('.rs-btn').click();
     cy.get('[href="/minhas-postagens"]').click();
-    cy.get(':nth-child(2) > .post-buttons > .delete-button').click();
-    cy.get('[href="/home"]').click();
-    cy.get('[href="/minhas-postagens"]').click();
     cy.get('[href="/home"]').click();
     cy.get('.formPostagem').click();
     cy.get('.rs-btn').click();
-    cy.get('[href="/minhas-postagens"]').click();
     cy.get('.rs-navbar-right > .rs-navbar-item > .rs-navbar-item-icon').click();
     /* ==== End Cypress Studio ==== */
   });
 })
+
